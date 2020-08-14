019cfab9a04fab614de9a961e580535001b83962
keithtang93@gmail.com <keithtang93@gmail.com>
2020-08-14 14:09:52 +0800

    Added Service Layer Department Head( Delegation,Approve,Reject Req)

---
+++ Service/InventoryService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Team7_StationeryStore.Database;
+using Team7_StationeryStore.Models;
+
+namespace Team7_StationeryStore.Service
+{
+    public class InventoryService
+    {
+        protected StationeryContext dbcontext;
+
+        public InventoryService(StationeryContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+        public List<Inventory> retrieveCatalogue()
+        {
+            return dbcontext.inventories.ToList();
+        }
+    }
+}
