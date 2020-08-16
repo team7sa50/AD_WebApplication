aa90620e7183ed2ef941737e414b033110810309
tess <tess>
2020-08-16 13:25:23 +0800

    Retrieval Service Created

--- Controllers/StationeryStoreController.cs
+++ Controllers/StationeryStoreController.cs
@@ -5,16 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Team7_StationeryStore.Database;
 using Team7_StationeryStore.Models;
+using Team7_StationeryStore.Service;
 
 namespace Team7_StationeryStore.Controllers
 {
     public class StationeryStoreController : Controller
     {
         protected StationeryContext dbcontext;
+        protected RetrievalService rservice;
 
-        public StationeryStoreController(StationeryContext dbcontext)
+        public StationeryStoreController(StationeryContext dbcontext, RetrievalService rservice)
         {
             this.dbcontext = dbcontext;
+            this.rservice = rservice;
         }
 
         public IActionResult Index()
@@ -22,101 +25,24 @@ namespace Team7_StationeryStore.Controllers
             System.Diagnostics.Debug.WriteLine("Reached Stationery Store Controller");
             return RedirectToAction("viewRetrieval", "StationeryStore");
         }
+        
 
         public IActionResult viewRetrieval()
         {
+            //Replace with the selected Requisitions from RequisitionController
             List<Requisition> selectedReq = (from x in dbcontext.requisitions
                                             select x).ToList();
 
-            //Contains all the Requisition Details 
-            List<RequisitionDetail> selectedReqDetail = new List<RequisitionDetail>(); 
-            
-            foreach (Requisition r in selectedReq)
-            {
-                List<RequisitionDetail> rds = (from x in dbcontext.requisitionDetails
-                                               where x.Requisition == r
-                                               select x).ToList();
-                foreach (RequisitionDetail rd in rds)
-                {
-                    selectedReqDetail.Add(rd);
-                }
-            }
-
-            //Group by unqiue items and total count
-            Dictionary<string, int> totalItemQty = new Dictionary<string, int>();
-            Dictionary<string, List<RequisitionDetail>> reqPerIt = new Dictionary<string, List<RequisitionDetail>>();
-          
-            foreach (RequisitionDetail rd in selectedReqDetail)
-            {
-                string item = rd.Inventory.description;
-                if (!totalItemQty.ContainsKey(item))
-                {
-                    totalItemQty.Add(item, rd.RequestedQty);
-                    reqPerIt.Add(item, new List<RequisitionDetail>());
-                    reqPerIt[item].Add(rd);
-                }
-                else if(totalItemQty.ContainsKey(item))
-                {
-                    int qty = totalItemQty[item];
-                    qty += rd.RequestedQty;
-                    totalItemQty[item] = qty;
-                    reqPerIt[item].Add(rd);
-                }
-            }
+            //Retrieval List Generation Starts here
+            List<RequisitionDetail> selectedReqDetail = rservice.getRequisitionDetail(selectedReq);          
+            Dictionary<string, int> totalItemQty = rservice.getTotalQtyPerItem(selectedReqDetail);
+            Dictionary<string, List<RequisitionDetail>> reqPerIt = rservice.getReqDetailPerItem(selectedReqDetail);
             ViewData["totalItemQty"] = totalItemQty;
-            recommendQty(reqPerIt);
-
-            System.Diagnostics.Debug.WriteLine("Start getting requisitions for one item");
-            Dictionary<string, List<RequisitionDetail>> reqPerDept = new Dictionary<string, List<RequisitionDetail>>();
-            string deptName; 
-            //loop through each item - requisition detail pair 
-            foreach(var rq in reqPerIt)
-            {
-                foreach (var r in rq.Value)
-                {
-                    deptName = r.Requisition.Employee.Departments.DeptName;
-                    if (!reqPerDept.ContainsKey(deptName))
-                    {
-                        //find 
-                        reqPerDept.Add(deptName, new List<RequisitionDetail>());
-                        reqPerDept[deptName].Add(r);
-                    }
-                    else if (reqPerDept.ContainsKey(deptName))
-                    {
-                        reqPerDept[deptName].Add(r);
-                    }
-                }
-            }
+            rservice.recommendQty(reqPerIt);
+            Dictionary<string, List<RequisitionDetail>> reqPerDept = rservice.getReqPerDeptPerItem(reqPerIt);
             ViewData["reqPerDept"] = reqPerDept;
             return View();        
         }
-
-        public void recommendQty(Dictionary<string, List<RequisitionDetail>> reqPerIt)
-        {
-
-            int itemQty;
-            int itemNeeded; 
-            foreach(var r in reqPerIt)
-            {
-                Inventory item = (from i in dbcontext.inventories
-                                  where i.description == r.Key
-                                  select i).FirstOrDefault();
-                itemQty = item.stock;
-                foreach(var rd in r.Value)
-                {
-                    itemNeeded = rd.RequestedQty;
-                    if (itemQty >= itemNeeded)
-                    {
-                        rd.DistributedQty = itemNeeded;
-                    }
-                    else if (itemQty < itemNeeded)
-                    {
-                        rd.DistributedQty = itemQty;
-                    }
-                    dbcontext.SaveChanges();
-                }
-            };
-
         }
     }
 }
---
+++ Service/RetrievalService.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Team7_StationeryStore.Database;
+using Team7_StationeryStore.Models;
+using Team7_StationeryStore.Models.Requisitions;
+
+namespace Team7_StationeryStore.Service
+{
+    public class RetrievalService
+    {
+        protected StationeryContext dbcontext;
+        public RetrievalService(StationeryContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+        public List<RequisitionDetail> getRequisitionDetail(List<Requisition> selectedReq)
+        {
+            List<RequisitionDetail> selectedReqDetail = new List<RequisitionDetail>();
+
+            foreach (Requisition r in selectedReq)
+            {
+                List<RequisitionDetail> rds = (from x in dbcontext.requisitionDetails
+                                               where x.Requisition == r
+                                               select x).ToList();
+                foreach (RequisitionDetail rd in rds)
+                {
+                    selectedReqDetail.Add(rd);
+                }
+            }
+
+            return selectedReqDetail;
+        }
+
+        public Dictionary<string, int> getTotalQtyPerItem(List<RequisitionDetail> selectedReqDetail)
+        {
+            Dictionary<string, int> totalItemQty = new Dictionary<string, int>();
+            
+            foreach (RequisitionDetail rd in selectedReqDetail)
+            {
+                string item = rd.Inventory.description;
+                if (!totalItemQty.ContainsKey(item))
+                {
+                    totalItemQty.Add(item, rd.RequestedQty);
+                }
+                else if (totalItemQty.ContainsKey(item))
+                {
+                    int qty = totalItemQty[item];
+                    qty += rd.RequestedQty;
+                    totalItemQty[item] = qty;
+                }
+            }
+
+            return totalItemQty;
+
+        }
+
+        public Dictionary<string, List<RequisitionDetail>> getReqDetailPerItem(List<RequisitionDetail> selectedReqDetail)
+        {
+            Dictionary<string, List<RequisitionDetail>> reqPerIt = new Dictionary<string, List<RequisitionDetail>>();
+
+            foreach (RequisitionDetail rd in selectedReqDetail)
+            {
+                string item = rd.Inventory.description;
+                if (!reqPerIt.ContainsKey(item))
+                {
+                    reqPerIt[item].Add(rd);
+                }
+                else if (reqPerIt.ContainsKey(item))
+                {
+                    reqPerIt[item].Add(rd);
+                }
+            }
+
+            return reqPerIt;
+
+        }
+
+        public void recommendQty(Dictionary<string, List<RequisitionDetail>> reqPerIt)
+        {
+
+            int itemQty;
+            int itemNeeded;
+            foreach (var r in reqPerIt)
+            {
+                Inventory item = (from i in dbcontext.inventories
+                                  where i.description == r.Key
+                                  select i).FirstOrDefault();
+                itemQty = item.stock;
+                foreach (var rd in r.Value)
+                {
+                    itemNeeded = rd.RequestedQty;
+                    if (itemQty >= itemNeeded)
+                    {
+                        rd.DistributedQty = itemNeeded;
+                    }
+                    else if (itemQty < itemNeeded)
+                    {
+                        rd.DistributedQty = itemQty;
+                    }
+                    dbcontext.SaveChanges();
+                }
+            };
+        }
+
+        public Dictionary<string, List<RequisitionDetail>> getReqPerDeptPerItem(Dictionary<string, List<RequisitionDetail>> reqPerIt)
+        {
+            Dictionary<string, List<RequisitionDetail>> reqPerDept = new Dictionary<string, List<RequisitionDetail>>();
+            string deptName;
+            //loop through each item - requisition detail pair 
+            foreach (var rq in reqPerIt)
+            {
+                foreach (var r in rq.Value)
+                {
+                    deptName = r.Requisition.Employee.Departments.DeptName;
+                    if (!reqPerDept.ContainsKey(deptName))
+                    {
+                        //find 
+                        reqPerDept.Add(deptName, new List<RequisitionDetail>());
+                        reqPerDept[deptName].Add(r);
+                    }
+                    else if (reqPerDept.ContainsKey(deptName))
+                    {
+                        reqPerDept[deptName].Add(r);
+                    }
+                }
+            }
+            return reqPerDept;
+        }
+
+            public void renameThisLater()
+        {
+
+
+            //Group by unqiue items and total count
+            Dictionary<string, int> totalItemQty = getTotalQtyPerItem(selectedReqDetail);
+            Dictionary<string, List<RequisitionDetail>> reqPerIt = getReqDetailPerItem(selectedReqDetail);
+
+            
+            ViewData["totalItemQty"] = totalItemQty;
+            recommendQty(reqPerIt);
+
+            Dictionary<string, List<RequisitionDetail>> reqPerDept = getReqPerDeptPerItem(reqPerIt);
+            
+        }
+    }
+}
