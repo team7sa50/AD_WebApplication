7d8df061af825560eb61a056c46b00bb85153c6f
ACTantzj <39021092+ACTantzj@users.noreply.github.com>
2020-08-22 08:20:45 +0800

    Change the API accesible using HTTP beside using HTTPS

--- Startup.cs
+++ Startup.cs
@@ -54,7 +54,7 @@ namespace Team7_StationeryStore
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
